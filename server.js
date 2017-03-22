1134113d06b9eaf0e3ce66a942e6c868d1a926c7
chelle tuerk <michelletuerk@gmail.com>
2017-03-22 16:38:38 -0600

    server-side...check

--- server.js
+++ server.js
@@ -6,13 +6,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-
-
-
-
-// =================================================================
-// app setup & configuration =======================================
-// =================================================================
+const jwt = require('jsonwebtoken');
+const config = require('dotenv').config();
 
 app.locals.trains = [
   { id: 1, line: 'green', status: 'running' },
@@ -21,25 +16,84 @@ app.locals.trains = [
   { id: 4, line: 'orange', status: 'maintenance' }
 ];
 
-// Use body parser so we can get info from POST/URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+if (!config.CLIENT_SECRET || !config.USERNAME || !config.PASSWORD)
+{
+  throw 'Make sure you have a CLIENT_SECRET, USERNAME, and PASSWORD in your .env file'
+}
+app.set('secretKey', config.CLIENT_SECRET);
 
+app.get('/api/v1/trains', (request, response) => {
+  response.send(app.locals.trains);
+});
 
+const checkAuth = (request, response, next) => {
+  const token = request.body.token ||
+                request.param('token') ||
+                request.headers['authorization'];
 
-// =================================================================
-// API Endpoints ===================================================
-// =================================================================
+  if (token) {
+    jwt.verify(token, app.get('secretKey'), (error, decoded) => {
 
-// This is all you baby!
+      if (error) {
+        return response.status(403).send({
+          success: false,
+          message: 'Invalid authorization token.'
+        });
+      }
+      else {
+        request.decoded = decoded;
+        next();
+      }
+    })
+  }
 
+  else {
+    return response.status(403).send({
+      success: false,
+      message: 'You must be authorized to hit this endpoint'
+    });
+  }
+}
 
+app.patch('/api/v1/trains/:id', checkAuth, (request, response) => {
+  const { train } = request.body;
+  const { id } = request.params;
+  const index = app.locals.trains.findIndex((m) => m.id == id);
 
+  if (index === -1) { return response.sendStatus(404); }
 
-// =================================================================
-// start the server ================================================
-// =================================================================
+  const originalTrain = app.locals.trains[index];
+  app.locals.trains[index] = Object.assign(originalTrain, train);
+
+  return response.json(app.locals.trains);
+});
+
+app.post('/authenticate', (request, response) => {
+  const user = request.body;
+
+  if (user.username !== config.USERNAME || user.password !== config.PASSWORD) {
+    response.status(403).send({
+      success: false,
+      message: 'Invalid Credentials'
+    });
+  }
+
+  // If the credentials are accurate, create a token and send it back
+  else {
+    let token = jwt.sign(user, app.get('secretKey'), {
+      expiresIn: 172800 // expires in 48 hours
+    });
+
+    response.json({
+      success: true,
+      username: user.username,
+      token: token
+    });
+  }
+});
 
 app.listen(3001);
 console.log('Listening on http://localhost:3001');
